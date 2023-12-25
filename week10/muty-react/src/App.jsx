1742b6851fdf5a12a1044e7cf5ba44e0fcb6ef19
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2023-12-25 10:43:05 +0800

    learn hooks in react js

--- week10/muty-react/src/App.jsx
+++ week10/muty-react/src/App.jsx
@@ -1,13 +1,46 @@
-import Student from "./Student"
+import { useState } from "react";
+import Student from "./Student";
 
 function App() {
+  const [name, setName] = useState("muty");
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["Learn ReactJS", "Learn React pokoknya"]);
+
+const handleClick = () => {
+  setCount(count + 1);
+}
+ 
   return (
     <>
       <Student name="Eddy" age={30} gender="male" />
       <Student name="Afista" age={27} gender="male" />
       <Student name="Ganjar" age={25} gender="male" />
+
+      <div>
+        <h1>{name}</h1>
+        <button onClick={() => setName("cut muty ahmad")}>
+          Change name into full name
+        </button>
+        <button onClick={() => setName("muty")}>Change back</button>
+      </div>
+
+      <div>
+        <h1>Count : {count}</h1>
+        <button onClick={handleClick}>count</button>
+      </div>
+
+      <div>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <button onClick={() => setTodos([...todos, "Learn NextJS"])}>
+        Add Todo
+      </button>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
