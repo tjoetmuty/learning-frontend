ff48b13459d2a0a946ffdb97615d7bc5d95af200
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-01-03 10:27:01 +0800

    create custom hooks

--- week10/muty-react/src/Reactrouter.jsx
+++ week10/muty-react/src/Reactrouter.jsx
@@ -1,26 +1,42 @@
 import { Routes, Route, Link } from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import { ContactPage } from "./pages/ContactPage";
+    // <Routes>
+    //   <Route path="/" element={<Homepage />} />
+    //   <Route path="/contact" element={<ContactPage />}>
+    //     <Route path="email" element={<p>email: tjmutyahmd27@gmail.com</p>}/>
+    //     <Route path="phone" element={<p>phone : +62 856 9606 5930</p>} />
+    //     <Route path="address" element={<p>address : palu</p>} />
+    //   </Route>
+    // </Routes>
 
 const Reactrouter = () => {
-  return (
-    <>
-    <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-        </ul>
-    </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/contact" element={<ContactPage />}>
-          <Route path="email" element={<p>email: tjmutyahmd27@gmail.com</p>}/>
-          <Route path="phone" element={<p>phone : +62 856 9606 5930</p>} />
-          <Route path="address" element={<p>address : palu</p>} />
-        </Route>
-      </Routes>
-    </>
-  );
+  const routes = useRoutes([
+    {
+      path:'/',
+      element: <Homepage/>
+    },
+    {
+      path:'/contact',
+      element: <ContactPage/>,
+      children: [
+        {
+        path: 'email',
+        element: <p>email: tjmutyahmd27@gmail.com</p>
+        },
+        {
+        path: 'phone',
+        element: <p>phone : +62 856 9606 5930</p>
+        },
+        {
+        path: 'address',
+        element: <p>address : palu</p>
+        },
+      ]
+    },
+  ])
+  return routes;
 };
 
 export default Reactrouter;
--- week10/muty-react/src/main.jsx
+++ week10/muty-react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import Reactrouter from './Reactrouter.jsx'
@@ -8,6 +8,12 @@ import Reactrouter from './Reactrouter.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+        </ul>
+    </nav>
     {/* <App /> */}
     <Reactrouter/>
     </BrowserRouter>
